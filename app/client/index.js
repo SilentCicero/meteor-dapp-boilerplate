70a8775bfccee3d50094d38ec883edaee55f5360
SilentCicero <silentcicero@outlook.com>
2015-10-19 15:57:53 -0400

    Cleanup

--- app/client/index.js
+++ app/client/index.js
@@ -23,9 +23,6 @@ Meteor.startup(function() {
     if(!web3.currentProvider)
         web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
     
-    // 
-    EthAccounts.init();
-    
     // Setup EthAccounts
     EthAccounts.init();
         
